3a4eafe46935f270e3680a5586549fdef8a9de75
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-03-28 19:19:50 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        int start = 2;
+        int finish = 4;
+        String path = "resources/data.txt";
+        String outputPath = "resources/resize_data.txt";
+
+        Scanner scanner = new Scanner(System.in);
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputPath));
+
+        List<String> strings = new ArrayList<>();
+
+        String line = "";
+        while (line != null) {
+                strings.add(line);
+                line = bufferedReader.readLine();
+            }
+        bufferedReader.close();
+
+        for (int i = 0; i < strings.size(); i++) {
+            if (i >= start && i <= finish) {
+                System.out.println(strings.get(i));
+                bufferedWriter.write(strings.get(i));
+                bufferedWriter.newLine();
+            }
+        }
+        bufferedWriter.close();
+
+        for (String s : strings) {
+            System.out.println(s);
+        }
+        }
     }
-}
